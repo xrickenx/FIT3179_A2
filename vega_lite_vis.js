c1a241848793a08bf5c238e78ee36dcd14bd8fcc
Ricken <70794235+xrickenx@users.noreply.github.com>
2024-10-06 10:22:35 +0800

    Add area stacked chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,7 @@
-var vg_1 = "map.vg.json"; 
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_1 = "map.vg.json";
+vegaEmbed("#map_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
+var vg_2 = "bar.vg.json";
+vegaEmbed("#area_chart", vg_2).then(function(result) {
+}).catch(console.error);
